4e75223e2159416b7bceb1da1aa4f8b37553902b
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2018-11-28 17:59:35 +0200

    initial commit

---
+++ wp-reset-opcache-aws-elb.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Plugin Name: AWS ELB OPCache reset
+ * Description: Resets OPCache on master and all ELB instances.
+ * Version: 0.0.1
+ * Author: Innocode
+ * Author URI: https://innocode.com
+ * Requires at least: 4.9.8
+ * Tested up to: 4.9.8
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace AWSELBOPCacheReset;
+
+use Aws\ElasticLoadBalancing\ElasticLoadBalancingClient;
+use Aws\Ec2\Ec2Client;
+use CacheTool\Adapter\FastCGI;
+use CacheTool\CacheTool;
+
+define( 'AWS_ELB_OPCACHE_RESET_VERSION', '0.0.1' );
+define( 'AWS_ELB_OPCACHE_RESET', 'aws_elb_opcache_reset' );
+
+if ( !defined( 'AWS_ELB_OPCACHE_RESET_PORT' ) ) {
+    define( 'AWS_ELB_OPCACHE_RESET_PORT', 8289 );
+}
+
+if ( !defined( 'AWS_ELB_OPCACHE_RESET_FALLBACK' ) ) {
+    define( 'AWS_ELB_OPCACHE_RESET_FALLBACK', '127.0.0.1' );
+}
+
+/**
+ * @return bool
+ */
+function is_enabled() {
+    return defined( 'AWS_ELB_OPCACHE_RESET_LOAD_BALANCER' )
+        && defined( 'AWS_ELB_OPCACHE_RESET_REGION' )
+        && function_exists( 'opcache_reset' )
+        && ini_get( 'opcache.enable' );
+}
+
+/**
+ * @return mixed|null
+ */
+function get_load_balancers() {
+    $elastic_load_balancing = new ElasticLoadBalancingClient( [
+        'region'  => AWS_ELB_OPCACHE_RESET_REGION,
+        'version' => 'latest',
+    ] );
+
+    return $elastic_load_balancing->describeLoadBalancers( [
+        'LoadBalancerNames' => [ AWS_ELB_OPCACHE_RESET_LOAD_BALANCER ], // Currently support one load balancer
+    ] )->get( 'LoadBalancerDescriptions' );
+}
+
+/**
+ * @param array $load_balancer
+ *
+ * @return \Aws\Result
+ */
+function get_ec2_load_balancer_instances( $load_balancer ) {
+    $ec2 = new Ec2Client( [
+        'region'  => AWS_ELB_OPCACHE_RESET_REGION,
+        'version' => 'latest',
+    ] );
+
+    return $ec2->describeInstances( [
+        'InstanceIds' => array_column( $load_balancer['Instances'], 'InstanceId' ),
+    ] );
+}
+
+/**
+ * @param string $host
+ *
+ * @return bool
+ */
+function reset_instance( $host ) {
+    $adapter = new FastCGI( "$host:" . AWS_ELB_OPCACHE_RESET_PORT );
+    $cache = CacheTool::factory( $adapter );
+    
+    return $cache->opcache_reset();
+}
+
+/**
+ * @param string $host
+ * @param int    $delay
+ */
+function schedule( $host, $delay = 0 ) {
+    wp_schedule_single_event( time() + $delay, AWS_ELB_OPCACHE_RESET, [
+        $host,
+    ] );
+}
+
+function reset() {
+    // Reset on master
+    opcache_reset();
+
+    if ( AWS_ELB_OPCACHE_RESET_FALLBACK ) {
+        schedule( AWS_ELB_OPCACHE_RESET_FALLBACK );
+    }
+
+    $load_balancers = get_load_balancers();
+
+    if ( !empty( $load_balancers ) && is_array( $load_balancers ) ) {
+        $x = intval( boolval( AWS_ELB_OPCACHE_RESET_FALLBACK ) );
+
+        foreach ( $load_balancers as $load_balancer ) {
+            $instances = get_ec2_load_balancer_instances( $load_balancer );
+
+            foreach ( $instances->get( 'Reservations' ) as $i => $reservation ) {
+                foreach ( $reservation['Instances'] as $j => $instance ) {
+                    schedule( $instance['PrivateIpAddress'], ( $i + $j + $x ) * MINUTE_IN_SECONDS );
+                }
+            }
+        }
+    }
+}
+
+function add_flush_button() {
+    if ( function_exists( 'mu_add_flush_button' ) && current_user_can( 'manage_options' ) ) {
+        mu_add_flush_button( __( 'OPCache' ), 'AWSELBOPCacheReset\reset' );
+    }
+}
+
+function load() {
+    add_flush_button();
+}
+
+if ( is_enabled() ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    add_action( 'plugins_loaded', 'AWSELBOPCacheReset\load' );
+    add_action( AWS_ELB_OPCACHE_RESET, 'AWSELBOPCacheReset\reset_instance' );
+}
